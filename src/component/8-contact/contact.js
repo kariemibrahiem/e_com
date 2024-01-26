5810cb6b439431253c78d8cab5c2ea329ad15540
kariemibrahiem <kariemibrahiem110@gmail.com>
2024-01-26 10:18:14 +0200

    update

---
+++ src/component/8-contact/contact.js
@@ -0,0 +1,17 @@
+import { Fragment } from "react";
+import End from "../5-end/end";
+import Foot from "../footer/foot";
+
+const Contact = () =>{
+    return(
+        <Fragment>
+            <div>
+                
+                <End />
+                <Foot />
+            </div>
+        </Fragment>
+    );
+}
+
+export default Contact;
---
+++ src/component/more_product/more_prod.js
@@ -0,0 +1,23 @@
+import { Fragment } from "react";
+import Card_a from "../3-card/single_card";
+import s from "../../material/shoes.png";
+import ball from "../../material/ball.png";
+import chair from "../../material/chair.png";
+import phone from "../../material/phone2.png";
+import shirt from "../../material/shirt.png";
+
+const Prod = () =>{
+    return(
+        <Fragment>
+            <div className="card_con">
+                <Card_a  title="phones" price="220$" img={phone}/> 
+                <Card_a  title="chair"  price="500$" img={chair}/> 
+                <Card_a  title="shirts" price="80$" img={shirt}/> 
+                <Card_a  title="shoes"  price="200$" img={s}/> 
+                <Card_a  title="balls"  price="100$" img={ball}/> 
+            </div>
+        </Fragment>
+    );
+}
+
+export default Prod;
